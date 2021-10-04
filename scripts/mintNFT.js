f2174ae84cde4b4701ee762d50d013dbb635af83
Rupesh Singh Ratore <rsrathore@isystango.com>
2021-10-04 19:48:37 +0530

    Added Scripts

--- scripts/mintNFT.js
+++ scripts/mintNFT.js
@@ -4,7 +4,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const data = require("../build/contracts/ERC721NFT.json");
 const abiArray = data.abi;
-const contract_address = "0x8c850114ef64dc4AC0235EC02F6fca146fD13f19";
+const contract_address = "0xE084B63eFb52873bdf57b46268c2aBDBa31162Ec";
 const mnemonic = process.env.MNEMONIC;
 const clientURL = process.env.CLIENT_URL;
 const provider = new HDWalletProvider(
@@ -19,32 +19,12 @@ async function mintNFT() {
     console.log("accounts:", accounts);
     console.log("contract_address", contract_address);
     const nft = new web3.eth.Contract(abiArray, contract_address);
-    await nft.methods
+    const mintData = await nft.methods
       .mintTicket(
-        JSON.stringify({
-          attributes: [
-            {
-              recipient: "Address",
-              typeID: "UInt64",
-              eventID: "UInt64",
-              section: "UInt64",
-              row: "UInt64",
-              seat: "UInt64",
-              ticketOwner: "String",
-              ticketprice: "UFix64",
-              originalTicketOwner: "String",
-              trait_type: "Color",
-              value: "Pink",
-            },
-          ],
-          description: "Event JSON",
-          image:
-            "https://ipfs.io/ipfs/QmYhvhwq5HN3pUcDbvJEs3M3TQxPmAahYLtFWigrNykHcW",
-          name: "Test NFT",
-        })
+        "https://ipfs.io/ipfs/QmUTF7sAqcpySq8VQAeCUxoBwXCkSLEffurzBQLG4VphcP"
       )
       .send({ from: accounts[0] });
-    console.log("Successfully minted NFT");
+    console.log("Successfully minted NFT " + JSON.stringify(mintData));
     // https://docs.openzeppelin.com/contracts/2.x/api/token/erc721#IERC721-balanceOf-address-
     // returns number of NFT's in owner's account
     const balance = await nft.methods.balanceOf(accounts[0]).call();
@@ -56,4 +36,43 @@ async function mintNFT() {
   }
 }
 
-mintNFT();
+async function transferToken() {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const nft = new web3.eth.Contract(abiArray, contract_address);
+    await nft.methods
+      .transferFrom(
+        accounts[0],
+        "0x73a28bEbe4efc836D06D252B9428613E81eA60b2",
+        1
+      )
+      .send({ from: accounts[0] });
+  } catch (err) {
+    console.log("error occured while calling deployed contract:", err);
+  }
+}
+
+async function balanceOf() {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const nft = new web3.eth.Contract(abiArray, contract_address);
+    const balance = await nft.methods.balanceOf(accounts[1]).call();
+    console.log("balance: ", balance);
+  } catch (err) {}
+}
+
+async function tokenURI(tokenId) {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const nft = new web3.eth.Contract(abiArray, contract_address);
+    const tokenData = await nft.methods.tokenURI(tokenId).call();
+    console.log("Token Data: ", tokenData);
+  } catch (err) {
+    console.log("Error :" + err);
+  }
+}
+
+//transferToken();
+//mintNFT();
+//balanceOf();
+tokenURI(1);
